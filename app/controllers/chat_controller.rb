d437ee07be25f04df1e88ab41e84af7b77226ad9
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2020-08-02 23:21:11 +0100

    connect message form to button

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,5 +1,6 @@
 class ChatController < ApplicationController
   before_action :authenticate!
+  attr_reader :current_user
 
   def show
     @messages = Message.order(created_at: :asc).last(20)
@@ -8,6 +9,7 @@ class ChatController < ApplicationController
   private
 
   def authenticate!
-    redirect_to login_path unless session[:username]
+    @current_user = session[:username]
+    redirect_to login_path unless @current_user
   end
 end
--- frontend/components/messages/messages.js
+++ frontend/components/messages/messages.js
@@ -1,2 +1,22 @@
+import { setCallback } from "../../client/chat";
 import "components/message/message";
 import "./messages.pcss";
+
+const scrollToButton = (element) => {
+  // eslint-disable-next-line
+  return (element.scrollTop = element.scrollheight);
+};
+
+const messages = document.querySelector(".js-messages");
+
+if (messages) {
+  const content = messages.querySelector(".js-messages--content");
+
+  scrollToButton(content);
+
+  setCallback((message) => {
+    content.insertAdjacentElement("beforeend", message);
+
+    scrollToButton(content);
+  });
+}
